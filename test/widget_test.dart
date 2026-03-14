b3a12b76493a5d364d2b6c85fb681bfc4c0b3441
harirajan-mec <harirajan.23ucs@mahendra.info>
2026-03-15 01:26:19 +0530

    final

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:find_it_student/main.dart';
+import 'package:find_it/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
